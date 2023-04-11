7042c72231893bed10a9f6eee46121a44891c55a
CDS-admin1 <csouthon44@gmail.com>
2023-04-11 17:37:47 +0100

    Creating Discover module

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,5 +1,6 @@
 import { HiOutlineHashtag, HiOutlineHome, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
 
+// Array of objects with two key pairs
 export const genres = [
   { title: 'Pop', value: 'POP' },
   { title: 'Hip-Hop', value: 'HIP_HOP_RAP' },
